3e9169470bf50524100805a19b12d29ca13cbb91
takoserver <96359093+tako0614@users.noreply.github.com>
2025-07-11 10:31:31 +0900

    feat: add service worker caching

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'tetris-cache-v1';
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/vite.svg',
+  '/grid-pattern.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        return res;
+      });
+    })
+  );
+});
