5bd69f1ac3aea5f7934cb50755e54b50c9e4f92d
takoserver <96359093+tako0614@users.noreply.github.com>
2025-09-05 02:52:05 +0000

    fix: add vite-plugin-singlefile for improved build output handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite'
+import  { viteSingleFile } from 'vite-plugin-singlefile'
 export default defineConfig({
-  plugins: [solid(),tailwindcss()],
+  plugins: [solid(),tailwindcss(),viteSingleFile()],
+    build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000, // 非常に大きく設定してすべてインライン化
+  }
 })
